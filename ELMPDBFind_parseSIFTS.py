a5fdfd961a271c785c873ce0fa70b4642f95a5c8
Nicolas Palopoli <nicopalo@gmail.com>
2016-03-31 16:54:15 -0300

    [DONE] Added output files with UniProt mapping

--- ELMPDBFind_parseSIFTS.py
+++ ELMPDBFind_parseSIFTS.py
@@ -6,7 +6,7 @@
   File name: parseSIFTS_PDBssSEQATOM.py
   Author: Nicolas Palopoli
   Date created: 2015/10/30
-  Date last modified: 2015/11/02
+  Date last modified: 2016/03/08
   Python Version: 2.7
 '''
 
@@ -32,25 +32,23 @@ except IOError:
 
 def readELMPDBFind(infile):
   '''Store list of PDB where ELM instance is found'''
-  listpdbfind = csv.DictReader(filter(lambda row: row[0]!='#', infile), delimiter='\t', quotechar='"', fieldnames=("Accession","PDB","CHAIN","SEQRESELM","SEQRESFLANK","SEQATOMELM","SEQATOMFLANK"))
+  listpdbfind = csv.DictReader(filter(lambda row: row[0]!='#', infile), delimiter='\t', quotechar='"', fieldnames=("Accession","PDB","CHAIN","SEQPDBSSELM","SEQPDBSSFLANK","SEQPDBSSFLANKN","SEQRESELM","SEQRESFLANK","SEQRESFLANKN","SEQATOMELM","SEQATOMFLANK","SEQATOMFLANKN"))
   return listpdbfind
 #  listfile = csv.DictReader(filter(lambda row: row[0]!='#', infile), delimiter='\t', quotechar='"')
 #  return listfile
 
 '''
-npalopoli@antares:~/20150924_ELM-Struct/ELMPDBFind$ ls ELMPDBFind_files/*ELMI000004*
-ELMPDBFind_files/ELMPDBFind_ELMI000004.tsv
 npalopoli@antares:~/20150924_ELM-Struct/ELMPDBFind$ head ELMPDBFind_files/ELMPDBFind_ELMI000004.tsv
 #NAM:sp|P97764|WBP1_MOUSE|ELMI000004|LIG|LIG_WW_1
 #SEQ:MARASSRNSSEEAWGSLQAPQQQQSPAASSLEGAIWRRAGTQTRALDTILYHPQQSHLLRELCPGVNTQPYLCETGHCCGETGCCTYYYELWWFWLLWTVLILFSCCCAFRHRRAKLRLQQQQRQREINLLAYHGACHGAGPVPTGSLLDLRLLSAFKPPAYEDVVHHPGTPPPPYTVGPGYPWTTSSECTRCSSESSCSAHLEGTNVEGVSSQQSALPHQEGEPRAGLSPVHIPPSCRYRRLTGDSGIELCPCPDSSEGEPLKEARASASQPDLEDHSPCALPPDSVSQVPPMGLASSCGDIP
 #ELM:PPPY
 #FLK:VVHHPGTPPPPYTVGPGYPW
-ELMI000004	3FBI	C	17	-	17	-
-ELMI000004	3FBI	A	17	-	17	-
-ELMI000004	1V55	A	499	-	499	-
-ELMI000004	1V55	N	499	-	499	-
-ELMI000004	4V4B	BM	108	-	108	-
-ELMI000004	1I8L	D	101	-	101	-
+#F20:EDVVHHPGTPPPPYTVGPGYPWTT
+#ELMAccession	PDBID	PDBChain	ELM-SeqPDBss	flanks-SeqPDBss	flank10-SeqPDBss	ELM-SEQRES	flanks-SEQRES	flank10-SEQRES	ELM-SEQATOM	flanks-SEQATOM	flank10-SEQATOM
+ELMI000004	3FBI	C	17	-	-	17	-	-	17	-	-
+ELMI000004	3FBI	A	17	-	-	17	-	-	17	-	-
+ELMI000004	1V55	A	499	-	-	499	-	-	499	-	-
+ELMI000004	1V55	N	499	-	-	499	-	-	499	-	-
 '''
 
 def parseELMPDBFind(listpdbfind):
@@ -58,17 +56,15 @@ def parseELMPDBFind(listpdbfind):
   pdbfindpos = {}
   for row in listpdbfind:
     key = '%s:%s:%s' % (row['Accession'],row['PDB'],row['CHAIN'].upper())  # index by ELMAccession:PDB:CHAIN
-    pdbfindpos[key] = [row['Accession'],row['PDB'],row['CHAIN'],row["SEQRESELM"],row["SEQRESFLANK"],row["SEQATOMELM"],row["SEQATOMFLANK"]]
+    pdbfindpos[key] = [row['Accession'],row['PDB'],row['CHAIN'],row["SEQPDBSSELM"],row["SEQPDBSSFLANK"],row["SEQPDBSSFLANKN"],row["SEQRESELM"],row["SEQRESFLANK"],row["SEQRESFLANKN"],row["SEQATOMELM"],row["SEQATOMFLANK"],row["SEQATOMFLANKN"]]
   return pdbfindpos
 
 def mapELMPDBFind(pdbfindpos, ELMpos):
   '''Get Primary_Acc for PDBs where ELM instance is found'''
   for key in pdbfindpos:
     if pdbfindpos[key][0] in ELMpos:
-#    if key in ELMpos:  # match ELMAccession
       selectELMpos = [ELMpos[pdbfindpos[key][0]][0], ELMpos[pdbfindpos[key][0]][1], ELMpos[pdbfindpos[key][0]][2]]
       pdbfindpos[key].extend(selectELMpos)
-#      pdbfindpos[key].append(ELMpos[pdbfindpos[key][0]][0])
   return pdbfindpos
 
 def printELMPDBFind(pdbfindpos):
@@ -85,7 +81,7 @@ def mapELMpositions(listELM):
   '''Make dict with {ELMAccession:[Primary_acc,Start,End,[PDB1,...,PDBN]]}'''
   ELMpos = {}
   for row in listELM:
-    ELMpos[row['Accession']] = [row['Primary_Acc'],row['Start'],row['End'],row['PDB'].split()]
+    ELMpos[row['Accession']] = [row['Primary_Acc'],int(row['Start']),int(row['End']),row['PDB'].split()]
   return ELMpos
 
 def readSIFTSpdbuniprot(infile):
@@ -93,56 +89,152 @@ def readSIFTSpdbuniprot(infile):
   listfile = csv.DictReader(filter(lambda row: row[0]!='#', infile),delimiter='\t', quotechar='"')
   return listfile
 
+'''
+def mapSIFTSpdbpositions(listSIFTS):
+  Make dict with {'SP_PRIMARY:PDB':[PDB,CHAIN,RES_BEG,RES_END,PDB_BEG,PDB_END,SP_PRIMARY,SP_BEG,SP_END]}
+  SIFTSpos = {}
+  for row in listSIFTS:
+    key = '%s:%s' % (row['SP_PRIMARY'],row['PDB'].upper())  # index by SP_PRIMARY:PDB
+    SIFTSpos[key] = [row['PDB'].upper(),row['CHAIN'].upper(),row['RES_BEG'],row['RES_END'],row['PDB_BEG'],row['PDB_END'],row['SP_PRIMARY'],row['SP_BEG'],row['SP_END']]
+  return SIFTSpos
+'''
 def mapSIFTSpdbpositions(listSIFTS):
   '''Make dict with {'SP_PRIMARY:PDB':[PDB,CHAIN,RES_BEG,RES_END,PDB_BEG,PDB_END,SP_PRIMARY,SP_BEG,SP_END]}'''
   SIFTSpos = {}
   for row in listSIFTS:
-    key = '%s:%s' % (row['SP_PRIMARY'],row['PDB'].upper())  # index by SP_PRIMARY:PDB
+#    key = '%s:%s:%s' % (row['SP_PRIMARY'],row['PDB'].upper(),row['CHAIN'])  # index by SP_PRIMARY:PDB:CHAIN
+    key = '%s:%s:%s:%s:%s' % (row['SP_PRIMARY'],row['PDB'].upper(),row['CHAIN'],row['SP_BEG'],row['SP_END'])  # index by SP_PRIMARY:PDB:CHAIN:SP_BEG:SP_END
     SIFTSpos[key] = [row['PDB'].upper(),row['CHAIN'].upper(),row['RES_BEG'],row['RES_END'],row['PDB_BEG'],row['PDB_END'],row['SP_PRIMARY'],row['SP_BEG'],row['SP_END']]
   return SIFTSpos
 
 def mapELMPDBFindposSIFTS(pdbfindpos,SIFTSpdbpos):
   '''Map ELMPDBFind start to Uniprot'''
   for key in pdbfindpos:
-    target = '{}:{}'.format(pdbfindpos[key][7],pdbfindpos[key][1])  # index by Accession:PDB
+    target = '{}:{}'.format(pdbfindpos[key][12],pdbfindpos[key][1])  # index by Accession:PDB
     if target in SIFTSpdbpos:
       continue  # program this!!
-   
+  
 '''
-ELMPDBFind_files/ELMPDBFind_ELMI000004.tsv
+pdbfindpos:
+0 row['Accession']
+1 row['PDB']
+2 row['CHAIN']
+3 row["SEQPDBSSELM"]
+4 row["SEQPDBSSFLANK"]
+5 row["SEQPDBSSFLANKN"]
+6 row["SEQRESELM"]
+7 row["SEQRESFLANK"]
+8 row["SEQRESFLANKN"]
+9 row["SEQATOMELM"]
+10row["SEQATOMFLANK"]
+11row["SEQATOMFLANKN"]]
+12(PrimaryAccession)
+13(PrimaryAccession_start)
+14(PrimaryAccession_end)
 npalopoli@antares:~/20150924_ELM-Struct/ELMPDBFind$ head ELMPDBFind_files/ELMPDBFind_ELMI000004.tsv
 #NAM:sp|P97764|WBP1_MOUSE|ELMI000004|LIG|LIG_WW_1
-#SEQ:MARASSRNSSEEAWGSLQAPQQQQSPAASSLEGAIWRRAGTQTRALDTILYHPQQSHLLRELCPGVNTQPYLCETGHCCGETGCCTYYYELWWFWLLWTVLILFSCCCAFRHRRAKLRLQQQQRQREINLLAYHGACHGAGPVPTGSLLDLRLLSAFKPPAYEDVVHHPGTPPPPYTVGPGYPWTTSSECTRCSSESSCSAHLEGTNVEGVSSQQSALPHQEGEPRAGLSPVHIPPSCRYRRLTGDSGIELCPCPDSSEGEPLKEARASASQPDLEDHSPCALPPDSVSQVPPMGLASSCGDIP
+#SEQ:MARASSRNSSEEAWGSLQAPQQQQSPAASSLEGAIWRRAGTQTRALDTILYHPQQSHLLRELCPGVNTQPYLCETGHCCGETGCCTYYYELWWFWLLWTVLILFSCCCAFRHRRAKLRLQQQQRQREINLLAYHGACHGAGPVPTGSLLDLRLLSAFKPP
 #ELM:PPPY
 #FLK:VVHHPGTPPPPYTVGPGYPW
-ELMI000004      3FBI    C       17      -       17      -
-ELMI000004      3FBI    A       17      -       17      -
-ELMI000004      1V55    A       499     -       499     -
+#F20:EDVVHHPGTPPPPYTVGPGYPWTT
+#ELMAccession   PDBID   PDBChain        ELM-SeqPDBss    flanks-SeqPDBss flank10-SeqPDBss        ELM-SEQRES      flanks-SEQRES   flank10-SEQRES  ELM-SEQATOM     flanks-SEQATOM  flank10-SEQATOM
+ELMI000004      3FBI    C       17      -       -       17      -       -       17      -       -
+ELMI000004      3FBI    A       17      -       -       17      -       -       17      -       -
+ELMI000004      1V55    A       499     -       -       499     -       -       499     -       -
+'''
+ 
+'''
+SIFTSpdbpos:
+row['SP_PRIMARY'], row['PDB'].upper(), row['CHAIN']  # index by SP_PRIMARY:PDB:CHAIN
+0 row['PDB']
+1 row['CHAIN']
+2 row['RES_BEG']
+3 row['RES_END']
+4 row['PDB_BEG']
+5 row['PDB_END']
+6 row['SP_PRIMARY']
+7 row['SP_BEG']
+8 row['SP_END']
+PDB     CHAIN   SP_PRIMARY      RES_BEG RES_END PDB_BEG PDB_END SP_BEG  SP_END
+101m    A       P02185  1       154     0       153     1       154
+102l    A       P00720  1       40      1       40      1       40
+102l    A       P00720  42      165     41      164     41      164
 '''
 
-'''
-PDB	CHAIN	SP_PRIMARY	RES_BEG	RES_END	PDB_BEG	PDB_END	SP_BEG	SP_END
-101m	A	P02185	1	154	0	153	1	154
-102l	A	P00720	1	40	1	40	1	40
-102l	A	P00720	42	165	41	164	41	164
-102m	A	P02185	1	154	0	153	1	154
-103l	A	P00720	1	40	1	40	1	40
-103l	A	P00720	44	167	41	164	41	164
-103m	A	P02185	1	154	0	153	1	154
-104l	A	P00720	1	44	1	44	1	44
-'''
+def OLD1_getELMPDBFindpos(pdbfindpos,SIFTSpdbpos):
+  '''Add Uniprot accession to ELMPDBFind list '''
+  for key in pdbfindpos:
+    target = '{}:{}'.format(pdbfindpos[key][12],pdbfindpos[key][1])  # index by PrimaryAccession:PDB
+    if target in SIFTSpdbpos:
+      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][13] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # PDB, Chain and Primary Accession match, within limits
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][4] and SIFTSpdbpos[target][8] >= pdbfindpos[key][5]:  # PDB, Chain and Primary Accession match, within limits
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2]:  # PDB, Chain and Primary Accession match, within limits
+        pdbfindpos[key].append(SIFTSpdbpos[target][6])
+      else:
+        pdbfindpos[key].append('-')
+#    pdbfindpos[key].append(SIFTSpdbpos)
+  return pdbfindpos
 
+def OLD2_getELMPDBFindpos(pdbfindpos,SIFTSpdbpos):
+  '''Add Uniprot accession to ELMPDBFind list '''
+  for key in pdbfindpos:
+    target = '{}:{}:{}'.format(pdbfindpos[key][12],pdbfindpos[key][1],pdbfindpos[key][2])  # index by PrimaryAccession:PDB:Chain
+    if target in SIFTSpdbpos:
+#    for k,v in SIFTSpdbpos.items() if k.startswith(target):
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]: # PDB, Chain and Primary Accession match
+        if SIFTSpdbpos[target][7] <= pdbfindpos[key][13] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # match within limits according to Primary Accession
+          pdbfindpos[key].extend((SIFTSpdbpos[target][6],SIFTSpdbpos[target][7],SIFTSpdbpos[target][8]))
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # PDB, Chain and Primary Accession match, within limits according to Primary Accession
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][13] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # PDB, Chain and Primary Accession match, within limits according to Primary Accession
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][4] and SIFTSpdbpos[target][8] >= pdbfindpos[key][5]:  # PDB, Chain and Primary Accession match, within limits
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2]:  # PDB, Chain and Primary Accession match, within limits
+
+#        pdbfindpos[key].extend((SIFTSpdbpos[target][6],SIFTSpdbpos[target][7],SIFTSpdbpos[target][8]))
+#        pdbfindpos[key].append(SIFTSpdbpos[target][6])
+        else:
+          pdbfindpos[key].extend((SIFTSpdbpos[target][6],SIFTSpdbpos[target][7],pdbfindpos[key][13],SIFTSpdbpos[target][8],pdbfindpos[key][14]))
+    else:  # PrimaryAccession:PDB:Chain not in SIFTS
+      pdbfindpos[key].extend(('-','-','-'))
+#      pdbfindpos[key].append('-')
+#    pdbfindpos[key].append(SIFTSpdbpos)
+  return pdbfindpos
 
 def getELMPDBFindpos(pdbfindpos,SIFTSpdbpos):
   '''Add Uniprot accession to ELMPDBFind list '''
   for key in pdbfindpos:
-    target = '{}:{}'.format(pdbfindpos[key][7],pdbfindpos[key][1])  # index by Accession:PDB
-    if target in SIFTSpdbpos:
-      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][7] and SIFTSpdbpos[target][7] <= pdbfindpos[key][4] and SIFTSpdbpos[target][8] >= pdbfindpos[key][5]:  # PDB, Chain and Primary Accession match, within limits
+    target = '{}:{}:{}'.format(pdbfindpos[key][12],pdbfindpos[key][1],pdbfindpos[key][2])  # index by PrimaryAccession:PDB:Chain
+    pdbfindpos[key].extend(('-','-','-'))  # in case PrimaryAccession:PDB:Chain not in SIFTS
+#    if target in SIFTSpdbpos:
+    for k in SIFTSpdbpos:
+#      if k.startswith(target):
+      if k.startswith(target) and int(k.split(':')[-2]) <= int(pdbfindpos[key][13]) and int(k.split(':')[-1]) >= int(pdbfindpos[key][14]):
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12]:  # PDB, Chain and Primary Accession match
+        if SIFTSpdbpos[k][0] == pdbfindpos[key][1] and SIFTSpdbpos[k][1] == pdbfindpos[key][2] and SIFTSpdbpos[k][6] == pdbfindpos[key][12] and int(SIFTSpdbpos[k][7]) <= int(pdbfindpos[key][13]) and int(SIFTSpdbpos[k][8]) >= int(pdbfindpos[key][14]): # PDB, Chain and Primary Accession match, within limits
+#        if SIFTSpdbpos[k][0] == pdbfindpos[key][1] and SIFTSpdbpos[k][1] == pdbfindpos[key][2] and SIFTSpdbpos[k][6] == pdbfindpos[key][12]: # PDB, Chain and Primary Accession match
+          if pdbfindpos[key][16] == '-' and int(SIFTSpdbpos[k][7]) <= int(pdbfindpos[key][13]) and int(SIFTSpdbpos[k][8]) >= int(pdbfindpos[key][14]):  # match within limits according to Primary Accession
+             pdbfindpos[key][15] = SIFTSpdbpos[k][6]
+             pdbfindpos[key][16] = SIFTSpdbpos[k][7]
+             pdbfindpos[key][17] = SIFTSpdbpos[k][8]
+#            pdbfindpos[key].extend((SIFTSpdbpos[k][6],SIFTSpdbpos[k][7],SIFTSpdbpos[k][8]))
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # PDB, Chain and Primary Accession match, within limits according to Primary Accession
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][13] and SIFTSpdbpos[target][8] >= pdbfindpos[key][14]:  # PDB, Chain and Primary Accession match, within limits according to Primary Accession
+#      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2] and SIFTSpdbpos[target][6] == pdbfindpos[key][12] and SIFTSpdbpos[target][7] <= pdbfindpos[key][4] and SIFTSpdbpos[target][8] >= pdbfindpos[key][5]:  # PDB, Chain and Primary Accession match, within limits
 #      if SIFTSpdbpos[target][0] == pdbfindpos[key][1] and SIFTSpdbpos[target][1] == pdbfindpos[key][2]:  # PDB, Chain and Primary Accession match, within limits
-        pdbfindpos[key].append(SIFTSpdbpos[target][6])
+
+#        pdbfindpos[key].extend((SIFTSpdbpos[target][6],SIFTSpdbpos[target][7],SIFTSpdbpos[target][8]))
+#        pdbfindpos[key].append(SIFTSpdbpos[target][6])
+          else:
+#            pdbfindpos[key].extend((SIFTSpdbpos[k][6],SIFTSpdbpos[k][7],pdbfindpos[key][13],SIFTSpdbpos[k][8],pdbfindpos[key][14]))
+            pdbfindpos[key] = pdbfindpos[key][0:15] + [SIFTSpdbpos[k][7], pdbfindpos[key][13], SIFTSpdbpos[k][8], pdbfindpos[key][14]]
+#      pdbfindpos[key].append('-')
 #    pdbfindpos[key].append(SIFTSpdbpos)
   return pdbfindpos
+
 
 def getPDBpos(ELMpos,SIFTSpdbpos):
   ''' '''
